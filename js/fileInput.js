37bd16b5499027f23675f5c92c9854ad2fe4d54f
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-06-13 17:01:48 -0500

    put all file input stuff here

--- js/fileInput.js
+++ js/fileInput.js
@@ -7,6 +7,11 @@ const customFileInput = document.querySelector("#customFileInput label");
 window.addEventListener("drop", event => event.preventDefault());
 window.addEventListener("dragover", event => event.preventDefault());
 
+realFileInput.addEventListener("input", ({ target }) => {
+    const file = target.files[0];
+    insert(file);
+});
+
 customFileInput.addEventListener("drop", ({ target, dataTransfer }) => {
     const file = dataTransfer.files[0];
     target.classList.remove("highlighted");
