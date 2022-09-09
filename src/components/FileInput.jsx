f3583194a9c66bba446094e680e23b7ba942bdfe
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-09-09 14:44:25 -0500

    switch to using react material icons + log invalid file message

--- src/components/FileInput.jsx
+++ src/components/FileInput.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Add } from '@mui/icons-material';
 
 export default function FileInput({ insertSVG, hasFile }) {
     async function addFile({ target }) {
         try {
             const file = target.files[0];
-            if (file.type !== "image/svg+xml") return;
+            
+            if (file.type !== "image/svg+xml") {
+                console.log("Invalid File.");
+                return;
+            }
+
             insertSVG(await file.text());
         } catch (error) {
             return;
@@ -14,7 +20,7 @@ export default function FileInput({ insertSVG, hasFile }) {
     return (
         <div className={hasFile ? "hidden" : ""}>
             <label htmlFor='fileInput' className='custom-file-input' title='Add a file.'>
-                <i className='material-icons'>add</i>
+                <Add/>
             </label>
             <input type="file" className='real-file-input' id='fileInput' onInput={addFile}/>
         </div>
